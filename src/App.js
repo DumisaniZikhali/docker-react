75b794ee5d0a009c966f80dff31eb9ae20797e5a
dumisani zikhali <dumisani.zikhali@tourafrika.co.za>
2021-05-16 16:55:10 +0200

    revised the docker file and AWS settings

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed again this time!
+          I HOPE IT WORKS THIS TIME AROUND!
         </p>
         <a
           className="App-link"
